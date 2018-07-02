438d43efc1c33565bf659acff60da24045d7d1ce
David Sherret <dsherret@gmail.com>
2018-07-02 18:02:19 -0400

    feat: #1 - Add position of cursor.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,5 +1,6 @@
 ﻿import React from "react";
 import MonacoEditor from "react-monaco-editor";
+import * as monacoEditor from "monaco-editor";
 
 export interface CodeEditorProps {
     onChange: (text: string) => void;
@@ -7,19 +8,52 @@ export interface CodeEditorProps {
     text: string;
 }
 
-export class CodeEditor extends React.Component<CodeEditorProps> {
+export interface CodeEditorState {
+    position: number;
+    lineNumber: number;
+    column: number;
+}
+
+export class CodeEditor extends React.Component<CodeEditorProps, CodeEditorState> {
+    constructor(props: CodeEditorProps) {
+        super(props);
+        this.state = {
+            position: 0,
+            lineNumber: 0,
+            column: 0
+        };
+        this.editorDidMount = this.editorDidMount.bind(this);
+    }
+
     render() {
         return (
             <div className="codeEditor">
-                <MonacoEditor
-                    width="100%"
-                    height="100%"
-                    value={this.props.text}
-                    theme="vs-dark"
-                    language="typescript"
-                    onChange={(text) => this.props.onChange(text)}
-                    options={{ automaticLayout: true, renderWhitespace: "all" }} />
+                <div className="editorContainer">
+                    <MonacoEditor
+                        width="100%"
+                        height="100%"
+                        value={this.props.text}
+                        theme="vs-dark"
+                        language="typescript"
+                        onChange={text => this.props.onChange(text)}
+                        editorDidMount={this.editorDidMount}
+                        options={{
+                            automaticLayout: true,
+                            renderWhitespace: "all"
+                        }} />
+                </div>
+                <div className="editorInfo">Pos {this.state.position}, Ln {this.state.lineNumber}, Col {this.state.column}</div>
             </div>
         );
+    }
+
+    editorDidMount(editor: monacoEditor.editor.IStandaloneCodeEditor) {
+        editor.onDidChangeCursorPosition(e => {
+            this.setState({
+                position: editor.getModel().getOffsetAt(e.position),
+                lineNumber: e.position.lineNumber,
+                column: e.position.column
+            });
+        });
     }
 }
