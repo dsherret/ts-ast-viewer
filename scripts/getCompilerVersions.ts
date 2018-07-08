e51874a86bf14414540f5f46063ebb5fb7e1ad25
David Sherret <dsherret@gmail.com>
2018-07-08 15:51:32 -0400

    chore: Use typescript@next on build server.

--- scripts/getCompilerVersions.ts
+++ scripts/getCompilerVersions.ts
@@ -8,11 +8,22 @@ export function getCompilerVersions() {
     const versions: { version: string; name: string; }[] = [];
 
     for (const key of Object.keys(dependencies)) {
+        if (key === "typescript-next") {
+            versions.push({ version: "@next", name: key });
+            continue;
+        }
+
         if (!keyRegEx.test(key))
             continue;
         const matches = versionRegEx.exec(dependencies[key]);
         versions.push({ version: matches[0], name: key });
     }
 
-    return versions.sort((a, b) => a.version > b.version ? -1 : 1);
+    return versions.sort((a, b) => {
+        if (a.version === "@next")
+            return 1;
+        if (b.version === "@next")
+            return -1;
+        return a.version > b.version ? -1 : 1
+    });
 }
