fb4adcf399e94886e1659e4865e1774966072486
David Sherret <dsherret@gmail.com>
2018-11-17 11:24:47 -0500

    fix: #33 - Some enum flag names are displayed twice.

---
+++ src/utils/__tests/getEnumFlagNames.test.ts
@@ -0,0 +1,23 @@
+import * as ts from "typescript";
+import { getEnumFlagNames } from "../getEnumFlagNames";
+
+describe("getEnumFlagNames", () => {
+    it("should get the flag names", () => {
+        expect(getEnumFlagNames(ts.SymbolFlags, 512)).toEqual([
+            "ValueModule",
+            "All",
+            "ParameterExcludes",
+            "Namespace",
+            "Module",
+            "FunctionScopedVariableExcludes",
+            "EnumMemberExcludes",
+            "ConstEnumExcludes",
+            "MethodExcludes",
+            "GetAccessorExcludes",
+            "SetAccessorExcludes",
+            "ModuleMember",
+            "ExportHasLocal",
+            "Classifiable"
+        ])
+    });
+});
--- src/utils/getEnumFlagNames.ts
+++ src/utils/getEnumFlagNames.ts
@@ -1,9 +1,9 @@
-import { CompilerApi, TypeFlags } from "../compiler";
-
 export function getEnumFlagNames(enumObj: any, flags: number) {
-    const allFlags = Object.keys(enumObj).map(k => enumObj[k]).filter(v => typeof v === "number") as number[];
+    const allFlags = Object.keys(enumObj)
+        .map(k => enumObj[k]).filter(v => typeof v === "number") as number[];
+    const matchedFlags = allFlags.filter(f => (f & flags) !== 0);
 
-    return allFlags
-        .filter(f => (f & flags) !== 0)
+    return matchedFlags
+        .filter((f, i) => matchedFlags.indexOf(f) === i)
         .map(f => enumObj[f]);
 }
