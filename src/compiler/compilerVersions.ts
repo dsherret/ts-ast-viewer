865c5c100d0c016af10a58afbcab3d884f7fbc74
David Sherret <dsherret@gmail.com>
2019-04-19 16:50:02 -0400

    chore: Really add ts 3.4. Forgot to check in this file.

--- src/compiler/compilerVersions.ts
+++ src/compiler/compilerVersions.ts
@@ -1,11 +1,12 @@
 /* tslint:disable */
 /* Automatically maintained from package.json. Do not edit! */
 
-export type compilerVersions = "3.3.3" | "3.2.4" | "3.1.6" | "3.0.3" | "2.9.2" | "2.8.4" | "2.7.2" | "2.6.2" | "2.5.3" | "2.4.2";
-export type compilerPackageNames = "typescript" | "typescript-3.2.4" | "typescript-3.1.6" | "typescript-3.0.3" | "typescript-2.9.2" | "typescript-2.8.4" | "typescript-2.7.2" | "typescript-2.6.2" | "typescript-2.5.3" | "typescript-2.4.2";
+export type compilerVersions = "3.4.2" | "3.3.3" | "3.2.4" | "3.1.6" | "3.0.3" | "2.9.2" | "2.8.4" | "2.7.2" | "2.6.2" | "2.5.3" | "2.4.2";
+export type compilerPackageNames = "typescript" | "typescript-3.3.3" | "typescript-3.2.4" | "typescript-3.1.6" | "typescript-3.0.3" | "typescript-2.9.2" | "typescript-2.8.4" | "typescript-2.7.2" | "typescript-2.6.2" | "typescript-2.5.3" | "typescript-2.4.2";
 
 export const compilerVersionCollection: { version: compilerVersions; packageName: compilerPackageNames; }[] = [
-    { version: "3.3.3", packageName: "typescript" },
+    { version: "3.4.2", packageName: "typescript" },
+    { version: "3.3.3", packageName: "typescript-3.3.3" },
     { version: "3.2.4", packageName: "typescript-3.2.4" },
     { version: "3.1.6", packageName: "typescript-3.1.6" },
     { version: "3.0.3", packageName: "typescript-3.0.3" },
@@ -22,6 +23,8 @@ export async function importCompilerApi(packageName: compilerPackageNames) {
     switch (packageName) {
         case "typescript":
             return await import("typescript");
+        case "typescript-3.3.3":
+            return await import("typescript-3.3.3");
         case "typescript-3.2.4":
             return await import("typescript-3.2.4");
         case "typescript-3.1.6":
@@ -51,6 +54,8 @@ export async function immportLibFiles(packageName: compilerPackageNames) {
     switch (packageName) {
         case "typescript":
             return await import("../resources/libFiles/typescript/index");
+        case "typescript-3.3.3":
+            return await import("../resources/libFiles/typescript-3.3.3/index");
         case "typescript-3.2.4":
             return await import("../resources/libFiles/typescript-3.2.4/index");
         case "typescript-3.1.6":
