40d12231c51cf1cb3b1ecaf1b42cbbd28f48182c
David Sherret <dsherret@gmail.com>
2021-08-26 18:43:31 -0400

    chore: amend

---
+++ site/src/resources/libFiles/typescript-4.0.5/lib.esnext.full.ts
@@ -0,0 +1,7 @@
+const fileData = {
+    fileName: `/lib.esnext.full.d.ts`,
+    // File text is copyright Microsoft Corporation and is distributed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+    text: `/// <reference no-default-lib="true"/>\n/// <reference lib="esnext" />\n/// <reference lib="dom" />\n/// <reference lib="webworker.importscripts" />\n/// <reference lib="scripthost" />\n/// <reference lib="dom.iterable" />`
+};
+
+export default fileData;
