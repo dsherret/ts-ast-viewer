5589097cbe4c8e96f29ae2201ecade38c1b4b77e
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2021-10-13 11:17:03 -0600

    feat: add powers of 2 to printed enum members if applicable (#93)

--- site/src/utils/getEnumFlagNames.ts
+++ site/src/utils/getEnumFlagNames.ts
@@ -1,9 +1,16 @@
 export function getEnumFlagNames(enumObj: any, flags: number) {
   const allFlags = Object.keys(enumObj)
-    .map(k => enumObj[k]).filter(v => typeof v === "number") as number[];
-  const matchedFlags = allFlags.filter(f => (f & flags) !== 0);
+    .map((k) => enumObj[k])
+    .filter((v) => typeof v === "number") as number[];
+  const matchedFlags = allFlags.filter((f) => (f & flags) !== 0);
 
   return matchedFlags
     .filter((f, i) => matchedFlags.indexOf(f) === i)
-    .map(f => enumObj[f]);
+    .map((f) => {
+      const power = Math.log2(f);
+      if (Number.isInteger(power)) {
+        return `${enumObj[f]} (2 ^ ${power})`;
+      }
+      return enumObj[f];
+    });
 }
