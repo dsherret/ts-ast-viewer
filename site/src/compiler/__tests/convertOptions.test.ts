f432850ec316e5e89e8e98ad1f378c68f11ca282
David Sherret <dsherret@gmail.com>
2022-11-27 15:07:51 -0500

    chore: update dependencies

--- site/src/compiler/__tests/convertOptions.test.ts
+++ site/src/compiler/__tests/convertOptions.test.ts
@@ -21,7 +21,7 @@ function doTest(fromKind: number, fromTarget: number, expectedKind: number, expe
   const apiTo = getCompilerApi(ScriptKindTo, ScriptTargetTo);
 
   expect(convertOptions(apiFrom, apiTo, {
-    compilerPackageName: "typescript-4.2.4",
+    compilerPackageName: "typescript",
     scriptKind: fromKind,
     scriptTarget: fromTarget,
     treeMode: 0,
@@ -29,7 +29,7 @@ function doTest(fromKind: number, fromTarget: number, expectedKind: number, expe
     showFactoryCode: true,
     showInternals: false,
   })).toEqual({
-    compilerPackageName: "typescript-4.2.4",
+    compilerPackageName: "typescript",
     scriptKind: expectedKind,
     scriptTarget: expectedTarget,
     treeMode: 0,
