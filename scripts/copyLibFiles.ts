e49c76a40a686ac4c01dc50748bf69fbe87eae43
David Sherret <dsherret@gmail.com>
2024-02-26 19:20:33 -0500

    Fix.

--- scripts/copyLibFiles.ts
+++ scripts/copyLibFiles.ts
@@ -1,7 +1,7 @@
 /* Copies the lib.d.ts files from node_modules into the src directory of the library for easy access */
 import { createMinifier } from "dts-minify";
 import * as fs from "fs";
-import * as glob from "glob";
+import { globSync } from "glob";
 import * as path from "path";
 import * as ts from "typescript";
 import { getCompilerVersions } from "./getCompilerVersions";
@@ -9,41 +9,39 @@ import { getCompilerVersions } from "./getCompilerVersions";
 const versions = getCompilerVersions();
 const minifier = createMinifier(ts);
 
-glob("./site/src/resources/libFiles/**/*.ts", (err, filesToDelete) => {
-  for (const filePath of filesToDelete) {
-    fs.unlinkSync(filePath);
+const filesToDelete = globSync("./site/src/resources/libFiles/**/*.ts");
+for (const filePath of filesToDelete) {
+  fs.unlinkSync(filePath);
+}
+
+const libFilesDir = "./site/src/resources/libFiles/";
+for (const version of versions) {
+  const filePaths = globSync(`./node_modules/${version.name}/lib/lib*.d.ts`);
+  const libVersionDir = libFilesDir + version.name + "/";
+  if (!fs.existsSync(libVersionDir)) {
+    fs.mkdirSync(libVersionDir);
   }
 
-  const libFilesDir = "./site/src/resources/libFiles/";
-  for (const version of versions) {
-    glob(`./node_modules/${version.name}/lib/lib*.d.ts`, (err, filePaths) => {
-      const libVersionDir = libFilesDir + version.name + "/";
-      if (!fs.existsSync(libVersionDir)) {
-        fs.mkdirSync(libVersionDir);
-      }
-
-      for (const filePath of filePaths) {
-        const newFilePath = libVersionDir + path.basename(filePath, ".d.ts") + ".ts";
-        const fileText = fs.readFileSync(filePath).toString("utf8");
-        fs.writeFileSync(
-          newFilePath,
-          `const fileData = {\n`
-            + `    fileName: \`/${path.basename(filePath)}\`,\n`
-            + `    // File text is copyright Microsoft Corporation and is distributed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)\n`
-            + `    text: \"${minifier.minify(fileText).replace(/\r?\n/g, "\\n").replace(/"/g, "\\\"")}\"\n`
-            + `};\n\n`
-            + `export default fileData;`,
-          { encoding: "utf8" },
-        );
-      }
-
-      fs.writeFileSync(
-        libVersionDir + "index.ts",
-        filePaths
-          .map(p => path.basename(p, ".d.ts"))
-          .map((p, i) => "export { default as export" + i + " } from \"./" + p + "\";").join("\n") + "\n",
-        { encoding: "utf8" },
-      );
-    });
+  for (const filePath of filePaths) {
+    const newFilePath = libVersionDir + path.basename(filePath, ".d.ts") + ".ts";
+    const fileText = fs.readFileSync(filePath).toString("utf8");
+    fs.writeFileSync(
+      newFilePath,
+      `const fileData = {\n`
+        + `    fileName: \`/${path.basename(filePath)}\`,\n`
+        + `    // File text is copyright Microsoft Corporation and is distributed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)\n`
+        + `    text: \"${minifier.minify(fileText).replace(/\r?\n/g, "\\n").replace(/"/g, "\\\"")}\"\n`
+        + `};\n\n`
+        + `export default fileData;`,
+      { encoding: "utf8" },
+    );
   }
-});
+
+  fs.writeFileSync(
+    libVersionDir + "index.ts",
+    filePaths
+      .map(p => path.basename(p, ".d.ts"))
+      .map((p, i) => "export { default as export" + i + " } from \"./" + p + "\";").join("\n") + "\n",
+    { encoding: "utf8" },
+  );
+}
