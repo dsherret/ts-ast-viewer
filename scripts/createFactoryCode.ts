7d8d2450a829efe5cecdd21128e2f5031d3e4fbd
David Sherret <dsherret@gmail.com>
2024-02-26 19:23:26 -0500

    More fixes

--- scripts/createFactoryCode.ts
+++ scripts/createFactoryCode.ts
@@ -1,20 +1,19 @@
 /* Copies the lib.d.ts files from node_modules into the src directory of the library for easy access */
 import * as fs from "fs";
-import * as glob from "glob";
+import { globSync } from "glob";
 import { generateCode } from "ts-factory-code-generator-generator";
 import { getCompilerVersions } from "./getCompilerVersions";
 
 const versions = getCompilerVersions();
 const factoryCodeDir = "./site/src/resources/factoryCode/";
 
-glob(`${factoryCodeDir}/*.ts`, (err, filesToDelete) => {
-  for (const filePath of filesToDelete) {
-    fs.unlinkSync(filePath);
-  }
+const filesToDelete = globSync(`${factoryCodeDir}/*.ts`);
+for (const filePath of filesToDelete) {
+  fs.unlinkSync(filePath);
+}
 
-  for (const version of versions) {
-    const code = generateCode(version.name);
-    const newFilePath = factoryCodeDir + `${version.name}.ts`;
-    fs.writeFileSync(newFilePath, `${code.replace(/\r?\n/g, "\n")}`);
-  }
-});
+for (const version of versions) {
+  const code = generateCode(version.name);
+  const newFilePath = factoryCodeDir + `${version.name}.ts`;
+  fs.writeFileSync(newFilePath, `${code.replace(/\r?\n/g, "\n")}`);
+}
