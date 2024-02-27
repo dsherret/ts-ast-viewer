1bfc409b58c767c262fd3dc79811a673e5a8f959
David Sherret <dsherret@gmail.com>
2024-02-26 19:09:29 -0500

    Revert.

--- shared/src/compilerVersions.generated.ts
+++ shared/src/compilerVersions.generated.ts
@@ -1,17 +1,8 @@
 // dprint-ignore-file
 /* Automatically maintained from sites/package.json. Do not edit! */
 
-export type CompilerPackageNames = "typescript-5.3.3" | "typescript-5.2.2" | "typescript-5.1.6" | "typescript-5.0.4" | "typescript-4.9.5" | "typescript-4.8.4" | "typescript-4.7.4" | "typescript-4.6.4" | "typescript-4.5.5" | "typescript-next";
-export type CompilerVersions = "5.3.3" | "5.2.2" | "5.1.6" | "5.0.4" | "4.9.5" | "4.8.4" | "4.7.4" | "4.6.4" | "4.5.5" | "@next (5.5.0-dev.20240226)";
+export type CompilerPackageNames = "typescript";
+export type CompilerVersions = "5.0.2";
 export const compilerVersionCollection: { version: CompilerVersions; packageName: CompilerPackageNames; }[] = [
-        { version: "5.3.3", packageName: "typescript-5.3.3" },
-        { version: "5.2.2", packageName: "typescript-5.2.2" },
-        { version: "5.1.6", packageName: "typescript-5.1.6" },
-        { version: "5.0.4", packageName: "typescript-5.0.4" },
-        { version: "4.9.5", packageName: "typescript-4.9.5" },
-        { version: "4.8.4", packageName: "typescript-4.8.4" },
-        { version: "4.7.4", packageName: "typescript-4.7.4" },
-        { version: "4.6.4", packageName: "typescript-4.6.4" },
-        { version: "4.5.5", packageName: "typescript-4.5.5" },
-        { version: "@next (5.5.0-dev.20240226)", packageName: "typescript-next" }
+        { version: "5.0.2", packageName: "typescript" }
     ];
