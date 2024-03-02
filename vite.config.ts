c8120f33f1c5e600c8e2dea6787bc7ba5a9a1eb8
David Sherret <dsherret@gmail.com>
2024-03-02 17:03:20 -0500

    Fixes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import monacoEditorPlugin from "vite-plugin-monaco-editor";
+import * as monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig({
   plugins: [
     react(),
-    // types are wrong for this package
-    (monacoEditorPlugin as any).default({}),
+    monacoEditorPlugin.default({}),
   ],
 });
