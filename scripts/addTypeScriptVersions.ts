55acce78ddc1d125cd70104bb1ddaaf8dcba94f9
David Sherret <dsherret@gmail.com>
2026-03-29 13:45:20 -0400

    chore: fix ci

--- scripts/addTypeScriptVersions.ts
+++ scripts/addTypeScriptVersions.ts
@@ -11,7 +11,7 @@ await npmInstallTypeScriptVersion("next");
 
 async function npmInstallTypeScriptVersion(version: string) {
   console.log(`Installing Typescript ${version}...`);
-  await $`deno install typescript-${version}@npm:typescript@${version}`
+  await $`deno install --save-exact typescript-${version}@npm:typescript@${version}`
     .cwd(path.resolve(import.meta.dirname!, "../"));
 }
 
